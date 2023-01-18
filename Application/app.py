ae0ba2cb1ec5cc119d1a6b3de5274c6419a84f24
ilies <ilies.chibane.2000@gmail.com>
2023-01-19 00:53:56 +0100

    UI + backend of the app

--- Application/app.py
+++ Application/app.py
@@ -1,2 +1,127 @@
-import gradio
+import gradio as gr
 import sri
+import files
+import pandas as pd
+
+#Notre fichier main qui lance l'application
+
+#fonction qui permet de définir les options du menu déroulant en fonction du type de SRI choisi
+def get_function(sri_type):
+    if sri_type == "Vectorial":
+        return gr.Dropdown.update(choices=["Scalar Product", "Cosine Measure", "Jaccard Measure"])
+    elif sri_type == "Boolean":
+        return gr.Dropdown.update(choices=["Base Boolean"])
+    elif sri_type == "Probabilistic":
+        return gr.Dropdown.update(choices=["BM25"])
+    elif sri_type == "Text Mining":
+        return gr.Dropdown.update(choices=["DBSCAN - Naive Bayes"])
+    elif sri_type == "Deep Learning":
+        return gr.Dropdown.update(choices=["LSTM - GRU"])
+
+# fonction qui permet d'ajouter des sliders pour le K et B dans le cas d'un SRI probabiliste
+def update_sliders(sri_type):
+    if sri_type == "Probabilistic": return gr.Slider.update(visible=True)
+    else: return gr.Slider.update(visible=False)
+
+# fonction qui permet de définir les requêtes en fonction du type de SRI choisi
+def get_set_queries(sri_type):
+    return gr.Dropdown.update(choices=sri.get_queries(sri_type))
+
+# fonction qui permet de définir si on affiche P@5 et P@10 en fonction du type de SRI choisi
+def update_displayed_metrics(sri_type):
+    if (sri_type == "Text Mining") or (sri_type == "Deep Learning"): return gr.Number.update(visible=False)
+    else: return gr.Number.update(visible=True)
+
+# fonction qui permet de définir si on affiche le bouton de calcul des métriques en fonction du type de SRI choisi
+def update_metrics_button(sri_type):
+    if (sri_type == "Boolean"): return gr.Button.update(visible=False)
+    else: return gr.Number.update(visible=True)
+
+# qui permet d'afficher le number box qui défini le nombre minimum de voisin pour DBSCAN si on choisit le SRI Text Mining
+def update_dbscan_minn(sri_type):
+    if (sri_type == "Text Mining"): return gr.Number.update(visible=True)
+    else: return gr.Number.update(visible=False)
+
+# qui permet d'afficher le slider qui défini la valeur du radius pour DBSCAN si on choisit le SRI Text Mining
+def update_dbscan_rad(sri_type):
+    if (sri_type == "Text Mining"): return gr.Slider.update(visible=True)
+    else: return gr.Slider.update(visible=False)
+
+# fonction qui permet de définir la liste des documents en fonction du type de stemmer choisi
+def update_docs(stem):
+    return gr.Dropdown.update(choices=files.get_docs(stem))
+
+# fonction qui permet de définir la liste des termes en fonction du type de stemmer choisi
+def update_terms(stem):
+    return gr.Dropdown.update(choices=files.get_terms(stem))
+
+with gr.Blocks() as demo:
+    gr.Markdown("<center><h1>Informarion Retrival Project</h1></center> <center><h2>Intelligent Information Retrival System</h2></center>")
+    with gr.Tab("Indexation"):
+        with gr.Row():
+            stemmer_selector = gr.Dropdown(["Porter", "Lancaster"], label="Stemmer") # menu déroulant pour choisir le type de stemmer
+            btn0 = gr.Button("Indexing") # bouton pour lancer l'indexation
+        with gr.Row():
+            nb_doc = gr.Textbox(label="Number of documents") # textbox pour afficher le nombre de documents
+            nb_words = gr.Textbox(label="Number of unique words") # textbox pour afficher le nombre de mots uniques
+            btn0.click(fn=files.get_stats, inputs=[stemmer_selector], outputs=[nb_doc, nb_words]) # on appelle la fonction get_stats qui va lancer l'indexation et afficher les résultats
+        with gr.Column(scale=1):
+            with gr.Row():
+                doc_selector = gr.Dropdown(label="Documents") # menu déroulant pour choisir les document à afficher
+                stemmer_selector.change(update_docs, stemmer_selector, doc_selector) # on appelle la fonction update_docs qui va mettre à jour la liste des documents en fonction du type de stemmer choisi
+                btn_doc = gr.Button("Show Document") # bouton pour afficher le fichier descripteur selon le document choisit
+            with gr.Row():
+                # dataframe pour afficher le fichier descripteur
+                fichier_descripteur = gr.DataFrame(pd.DataFrame(columns=["Teme", "Freq", "Poids"]), label="Fichier Descripteur")
+                # on appelle la fonction rech_fichier_descripteur qui va afficher le fichier descripteur selon le document choisi
+                btn_doc.click(fn=files.rech_fichier_descripteur, inputs=[doc_selector, stemmer_selector], outputs=[fichier_descripteur])
+        with gr.Column(scale=1):
+            with gr.Row():
+                term_selector = gr.Dropdown(label="Terms") # menu déroulant pour choisir les termes à afficher
+                stemmer_selector.change(update_terms, stemmer_selector, term_selector) # on appelle la fonction update_terms qui va mettre à jour la liste des termes en fonction du type de stemmer choisi
+                btn_term = gr.Button("Show Term") # bouton pour afficher le fichier inverse selon le terme choisi
+            with gr.Row():
+                # dataframe pour afficher le fichier inverse
+                fichier_inverse = gr.DataFrame(pd.DataFrame(columns=["Teme", "Freq", "Poids"]), label="Fichier inverse")
+                # on appelle la fonction rech_fichier_inverse qui va afficher le fichier inverse selon le terme choisi
+                btn_term.click(fn=files.rech_fichier_inverse, inputs=[term_selector, stemmer_selector], outputs=[fichier_inverse])
+    with gr.Tab("Appariement"):
+        with gr.Row():
+            with gr.Column(scale=1):
+                # menu déroulant pour choisir le type de SRI
+                sri_selector = gr.Dropdown(["Vectorial", "Boolean", "Probabilistic", "Text Mining", "Deep Learning"], label="SRI")
+
+                function_selector = gr.Dropdown(label="Function", visible=True) # menu déroulant pour choisir le type de fonction de similarité
+                sri_selector.change(get_function, sri_selector, function_selector) # on appelle la fonction get_function qui va mettre à jour la liste des fonctions en fonction du type de SRI choisi
+
+                query_selector = gr.Dropdown(label="Query", visible=True) # menu déroulant pour choisir la requête à afficher
+                sri_selector.change(get_set_queries, sri_selector, query_selector) # on appelle la fonction get_set_queries qui va mettre à jour la liste des requêtes en fonction du type de SRI choisi
+
+                b_value = gr.Slider(0.5, 0.75, 0.6, label="b value", visible=False) # slider pour choisir la valeur de b
+                sri_selector.change(update_sliders, sri_selector, b_value) # on appelle la fonction update_sliders qui va mettre à jour l'affichage du slider en fonction du type de SRI choisi
+                k_value = gr.Slider(1.2, 2, 1.5, label="k value", visible=False) # slider pour choisir la valeur de k
+                sri_selector.change(update_sliders, sri_selector, k_value) # on appelle la fonction update_sliders qui va mettre à jour l'affichage du slider en fonction du type de SRI choisi
+
+                radius = gr.Slider(0, 1, 0.6, label="Radius", visible=False) # slider pour choisir la valeur du rayon
+                sri_selector.change(update_dbscan_rad, sri_selector, radius) # on appelle la fonction update_dbscan_rad qui va mettre à jour l'affichage du slider en fonction du type de SRI choisi
+                min_neighbours = gr.Number(value= 200, label="Min Neighbours", visible=False) # nombre pour choisir le nombre de voisins minimum
+                sri_selector.change(update_dbscan_minn, sri_selector, min_neighbours) # on appelle la fonction update_dbscan_minn qui va mettre à jour l'affichage de la nombrebox en fonction du type de SRI choisi
+
+                btn1 = gr.Button("Compute") # bouton pour lancer le calcul de la similarité
+            with gr.Column(scale=3):
+                df_out = gr.DataFrame(pd.DataFrame(columns=["Document", "Score"]), label="Results") # dataframe pour afficher les résultats
+                # on appelle la fonction apply_sri qui va calculer la similarité selon le type de SRI choisi
+                btn1.click(fn=sri.apply_sri, inputs=[stemmer_selector, query_selector, function_selector, b_value, k_value, radius, min_neighbours], outputs=df_out)
+            with gr.Column(scale=1):
+                p = gr.Number(label="Precision") # nombrebox pour afficher la précision
+                p5 = gr.Number(label="P@5") # nombrebox pour afficher la précision à 5
+                sri_selector.change(update_displayed_metrics, sri_selector, p5) # on appelle la fonction update_displayed_metrics qui va mettre à jour l'affichage des nombrebox en fonction du type de SRI choisi
+                p10 = gr.Number(label="P@10") # nombrebox pour afficher la précision à 10
+                sri_selector.change(update_displayed_metrics, sri_selector, p10) # on appelle la fonction update_displayed_metrics qui va mettre à jour l'affichage des nombrebox en fonction du type de SRI choisi
+                metrics_out = [p, p5, p10, gr.Number(label="Recall"), gr.Number(label="F measure"), gr.Plot()] # liste des nombrebox pour afficher les métriques
+                btn2 = gr.Button("Compute Metrics") # bouton pour lancer le calcul des métriques
+                sri_selector.change(update_metrics_button, sri_selector, btn2) # on appelle la fonction update_metrics_button qui va mettre à jour l'affichage du bouton en fonction du type de SRI choisi
+                btn2.click(fn=sri.metrics, inputs=[query_selector, df_out], outputs=metrics_out) # on appelle la fonction metrics qui va calculer les métriques selon le type de SRI choisi
+
+if __name__ == "__main__":
+    demo.launch()
--- Application/sri.py
+++ Application/sri.py
@@ -1,4 +1,118 @@
 import nltk
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import re
+import json
+import matplotlib
+
+from vectorial import scalar_product, cosine_measure, jaccard_measure
+from boolean import boolean_model
+from probabilistic import bm25
+from textmining import text_mining_sri, dbscan
+from deeplearning import get_result
+
+matplotlib.use('Agg')
+
+# fichier qui contient les fonctions qui permettent de lancer l'application 
+# et de définir les options du menu déroulant, et de calculer les métriques en fonction du type de SRI choisi
+
+
+################ importation des données ################ 
+info_queries = pd.read_csv("../csv_docs/info_queries.csv")
+info_queries = info_queries.drop("Unnamed: 0", axis=1)
+
+eval_df = pd.read_csv("../csv_docs/evaluation.csv")
+eval_df = eval_df.drop("Unnamed: 0", axis=1)
+
+bool_queriers = pd.read_csv("../csv_docs/bool_queries.csv")
+bool_queriers = bool_queriers.drop("Unnamed: 0", axis=1)
+
+df = pd.read_csv("../csv_docs/dataset_dbscan.csv")
+df.drop(columns = df.columns[0], axis = 1, inplace= True)
+#########################################################
+
+
+# Fonction qui permet de récupérer les requêtes en fonction du type de SRI choisi
+def get_queries(sri_type):
+    if sri_type == "Boolean": return list(bool_queriers["Code"].unique())
+    else : return list(info_queries["Query"].unique())
+
+# Fonction qui permet de calculer la précision de notre SRI par rapport à la requête
+def precision(query, eval_df, result, k):
+    eval_pk = eval_df.loc[eval_df["Query"] == query, "Document"].to_numpy() # Documents pertinents pour la requête
+    # Documents pertinents retournés par le SRI on récupère tout documents pour calculer la précision les 5 premiers pour p@5 et les 10 premiers pour p@10
+    result_pk = result.iloc[0:k]["Document"].to_frame() if k != 0 else result["Document"].to_frame() 
+    if k == 0: k = len(result_pk) if len(result_pk) != 0 else 1 # Si k = 0 on prend tous les documents retournés
+    return result_pk.isin(eval_pk).sum()[0] / k  # On calcule la précision
+
+# Fonction qui permet de calculer le rappel de notre SRI par rapport à la requête
+def recall(query, eval_df, result):
+    eval_pk = eval_df.loc[eval_df["Query"] == query, "Document"].to_numpy() # Documents pertinents pour la requête
+    doc_select = int(result.isin(eval_pk)["Document"].sum()) # Documents pertinents retournés par le SRI
+    return doc_select / len(eval_pk) if len(eval_pk) != 0 else 0 # On calcule le rappel 
+
+# Fonction qui permet de calculer la mesure F de notre SRI par rapport à la requête
+def f_measure(p, r):
+    return 2 * p * r / (p + r) if p + r != 0 else 0
+
+# Fonction qui permet de calculer la courbe de rappel précision de notre SRI par rapport à la requête
+def crp(query, result, eval_df):
+    eval_pk = eval_df.loc[eval_df["Query"] == query, "Document"].to_numpy() # Documents pertinents pour la requête
+    result_pk = result["Document"].to_frame() # Documents retournés par le SRI
+    l = len(result_pk) # Nombre de documents retournés par le SRI
+    k = result_pk.isin(eval_pk).sum()[0] # Nombre de documents pertinents retournés par le SRI
+    rp = [] # Liste qui contient les valeurs de la courbe de rappel précision
+    for i in range(1,l): # On calcule la précision pour les 1 premiers documents, puis pour les 2 premiers, puis pour les 3 premiers, etc...
+        pi = precision(query, eval_df, result, i) # On calcule la précision pour les i premiers documents
+        result_pk = result.iloc[0:i]["Document"].to_frame() # On récupère les i premiers documents retournés par le SRI
+        ki = result_pk.isin(eval_pk).sum()[0] # On calcule le nombre de documents pertinents parmi les i premiers documents retournés par le SRI
+        rp.append([pi, ki/k]) # On ajoute la valeur de la précision et du rappel pour les i premiers documents
+    rp = pd.DataFrame(rp, columns=["Precision", "Rappel"]) # On transforme la liste en dataframe
+    rpi = [] # Liste qui contient les valeurs de la courbe de rappel précision interpolée
+    j = 0 # On commence à 0
+    while j <= 1: # On va jusqu'à 1
+        p_max = rp.loc[rp["Rappel"] >= j]["Precision"].max() # On récupère la précision maximale pour un rappel supérieur ou égal à j
+        rpi.append([j, p_max]) # On ajoute la valeur de la précision et du rappel pour le rappel j
+        j += (1/l) # On incrémente j
+    rpi = pd.DataFrame(rpi, columns=["Rappel", "Precision"]) # On transforme la liste en dataframe
+    fig = plt.figure() # On crée la figure
+    plt.title("Courbe de rappel précision") # On ajoute le titre
+    plt.xlabel("Rappel") # On ajoute l'axe des abscisses
+    plt.ylabel("Précision") # On ajoute l'axe des ordonnées
+    plt.plot(rpi["Rappel"], rpi["Precision"]) # On ajoute la courbe de rappel précision interpolée
+    return fig
+
+# Fonction qui permet de calculer les métriques de notre SRI par rapport à la requête pour les retourner à notre utilisateur
+def metrics(query, result): 
+    p = precision(query, eval_df, result, 0) # On calcule la précision
+    p5 = precision(query, eval_df, result, 5) # On calcule la p@5
+    p10 = precision(query, eval_df, result, 10) # On calcule la p@10
+    r = recall(query, eval_df, result) # On calcule le rappel
+    f = f_measure(p, r) # On calcule la mesure F
+    plot = crp(query, result, eval_df) # On calcule la courbe de rappel précision
+    return p, p5, p10, r, f, plot
+
+# Fonction qui permet d'appliquer le SRI sélection par notre utilisateur à la requête qu'il a choisi
+def apply_sri(stem, query, sri, B=0, K=0, radius=0.6, min_neighbours=200):
+    if stem == "Lancaster" : fichier_inverse = pd.read_csv("../csv_docs/fichier_inverse_Lancaster.csv") # On récupère le fichier inverse
+    else : fichier_inverse = pd.read_csv("../csv_docs/fichier_inverse.csv") # On récupère le fichier inverse
+    fichier_inverse = fichier_inverse.drop("Unnamed: 0", axis=1) # On supprime la colonne inutile
+
+    # On applique le SRI sélectionné par notre utilisateur à la requête qu'il a choisi et on récupère les résultats
+    if sri == "Scalar Product": 
+        return scalar_product(query, fichier_inverse, info_queries)
+    elif sri == "Cosine Measure":
+        return cosine_measure(query, fichier_inverse, info_queries)
+    elif sri == "Jaccard Measure":
+        return jaccard_measure(query, fichier_inverse, info_queries)
+    elif sri == "Base Boolean":
+        return boolean_model(query, fichier_inverse, bool_queriers, stem)
+    elif sri == "BM25":
+        return bm25(query, fichier_inverse, info_queries, B, K)
+    elif sri == "DBSCAN - Naive Bayes":
+        return text_mining_sri(query, info_queries, df, fichier_inverse, radius, min_neighbours)
+    elif sri == "LSTM - GRU":
+        return get_result(query)
+    else:
+        return None
